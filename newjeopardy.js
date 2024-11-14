1c5ca872050f53f831267ee755b78495f6ef8d58
juggervex <159195587+juggervex@users.noreply.github.com>
2024-11-13 20:37:01 -0500

    Add files via upload

---
+++ newjeopardy.js
@@ -0,0 +1,118 @@
+const BASE_API_URL = "https://rithm-jeopardy.herokuapp.com/api/";
+const NUM_CATEGORIES = 6;
+const NUM_CLUES_PER_CAT = 5;
+
+
+let categories = [];
+
+
+async function getCategoryIds() {
+ 
+  let response = await axios.get(`${BASE_API_URL}categories`, {
+    params: { count: 100 }
+  });
+  let catIds = response.data.map(c => c.id);
+  return _.sampleSize(catIds, NUM_CATEGORIES);
+}
+async function getCategory(catId) {
+  let response = await axios.get(`${BASE_API_URL}category`, {
+    params: { id: catId }
+  });
+  let cat = response.data;
+  let randomClues = _.sampleSize(cat.clues, NUM_CLUES_PER_CAT).map(c => ({
+    question: c.question,
+    answer: c.answer,
+    showing: null
+  }));
+  return { title: cat.title, clues: randomClues };
+}
+
+async function fillTable() {
+  hideLoadingView();
+
+  
+  let $tr = $("<tr>");
+  for (let category of categories) {
+    $tr.append($("<th>").text(category.title));
+  }
+  $("#jeopardy thead").append($tr);
+
+  
+  $("#jeopardy tbody").empty();
+  for (let clueIdx = 0; clueIdx < NUM_CLUES_PER_CAT; clueIdx++) {
+    let $tr = $("<tr>");
+    for (let catIdx = 0; catIdx < NUM_CATEGORIES; catIdx++) {
+      $tr.append(
+        $("<td>")
+          .attr("id", `${catIdx}-${clueIdx}`)
+          .append($("<i>").addClass("fas fa-question-circle fa-3x"))
+      );
+    }
+    $("#jeopardy tbody").append($tr);
+  }
+}
+
+
+function handleClick(evt) {
+  let $tgt = $(evt.target);
+  let id = $tgt.attr("id");
+  let [catId, clueId] = id.split("-");
+  let clue = categories[catId].clues[clueId];
+
+  let msg;
+
+  if (!clue.showing) {
+    msg = clue.question;
+    clue.showing = "question";
+  } else if (clue.showing === "question") {
+    msg = clue.answer;
+    clue.showing = "answer";
+    $tgt.addClass("disabled");
+  } else {
+    
+    return;
+  }
+
+  
+  $tgt.html(msg);
+}
+
+function showLoadingView() {
+  
+  $("#jeopardy thead").empty();
+  $("#jeopardy tbody").empty();
+
+  
+  $("#spin-container").show();
+  $("#start")
+    .addClass("disabled")
+    .text("Loading...");
+}
+
+function hideLoadingView() {
+  $("#start")
+    .removeClass("disabled")
+    .text("Restart!");
+  $("#spin-container").hide();
+}
+
+async function setupAndStart() {
+  let isLoading = $("#start").text() === "Loading...";
+
+  if (!isLoading) {
+    showLoadingView();
+
+    let catIds = await getCategoryIds();
+
+    categories = [];
+
+    for (let catId of catIds) {
+      categories.push(await getCategory(catId));
+    }
+    fillTable();
+  }
+}
+$("#start").on("click", setupAndStart);
+$(async function() {
+  $("#jeopardy").on("click", "td", handleClick);
+});
